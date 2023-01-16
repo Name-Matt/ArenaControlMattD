006752aef7f4772977d8ec263d9130b9b83bed0e
Matt <md421@student.le.ac.uk>
2023-01-16 20:54:58 +0000

    annoyed is an understatement

--- Arduino/GUI_12_1/GUI_12_1.ino
+++ Arduino/GUI_12_1/GUI_12_1.ino
@@ -80,6 +80,7 @@ void doorOPENED_ISR() {  //what to do if door is open
   setLEDColour(255, 0, 0);  //set lights red
   //keep running this loop if ANY doors are open
   while (doorOpen == true) {
+    Serial.println("TRUE");
     //will break the while loop if the door is shut
     if ((digitalRead(DoorPins[0]) == LOW) && digitalRead(DoorPins[1]) == LOW) {
       doorOpen = false;
--- Arduino/Test/Test.ino
+++ Arduino/Test/Test.ino
@@ -1,13 +1,39 @@
+const int limitSwitchPin = 2;
+const int debounceDelay = 50;
+
+bool switchState;
+bool lastSwitchState = LOW;
+unsigned long lastDebounceTime = 0;
+
 void setup() {
-  // Initialize serial communication at 9600 baud
+  pinMode(limitSwitchPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(limitSwitchPin), test, FALLING);
   Serial.begin(9600);
 }
 
 void loop() {
-  // Read the value of pin A0
-  int sensorValue = analogRead(A0);
-  // Send the value over serial
-  Serial.println("Connected to Arena");
-  // Wait for a bit before reading the next value
+  // other code here
   delay(100);
+}
+
+void test() {
+  int reading = digitalRead(limitSwitchPin);
+  Serial.println("TEST");
+
+  if (reading != lastSwitchState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != switchState) {
+      switchState = reading;
+
+      if (switchState == LOW) {
+        // limit switch triggered event
+        Serial.println("Limit switch triggered!");
+      }
+    }
+  }
+
+  lastSwitchState = reading;
 }
